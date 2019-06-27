5e803d86d5ffe2a3c1ef94d95b4b7fa940f6641e
Friedrich Striewski <friedrich.striewski@bynder.com>
2019-06-27 08:33:19 +0200

    Assign parentId to paragraphs

--- src/data/Cvdata.tsx
+++ src/data/Cvdata.tsx
@@ -1,17 +1,24 @@
 import * as React from 'react';
-import { ID, ITextElement, ICV, INode, IHeader } from '../types';
+import { ID, ITextElement, ICV, INode, IHeader, Position } from '../types';
 import { Paragraph } from 'src/styles/Element';
+
+interface IPartialNode {
+  id: string;
+  x: number;
+  y: number;
+  col: Position;
+}
 
 interface IState {
   id: string;
   header: IHeader;
-  nodes:INode[];
+  nodes: IPartialNode[];
   paragraphs: ITextElement[];
 }
 
 interface IRenderProps {
   header: IHeader;
-  nodes:INode[];
+  nodes: IPartialNode[];
   paragraphs: ITextElement[];
   updatePosition: (id, pos) => void;
 }
@@ -31,39 +38,43 @@ export class CVState extends React.Component<IProps, IState> {
       header: null,
       id: null,
       nodes: [],
-      paragraphs: [],
+      paragraphs: []
     };
   }
 
   componentDidMount() {
     console.log(this.props.json);
-    
+
     const header = this.props.json.header || null;
     const nodes = this.destructureNodes(this.props.json);
     const paragraphs = this.destructureParagraphs(this.props.json);
     this.setState({
       header,
       nodes,
-      paragraphs,
+      paragraphs
     });
   }
 
-destructureParagraphs = (json: ICV) => {
-      if (json.nodes) {
-        const arr = json.nodes.map(node => node.paragraphs);
-        const merged = [].concat.apply([], arr);
-
-        return merged
-      }
-      return [];
+  destructureParagraphs = (json: ICV) => {
+    if (json.nodes) {
+      // add parent id to each paragraph for lookup
+      const arr = json.nodes.map(node =>
+        node.paragraphs.map(p => {
+          const parentId = node.id;
+          return { ...p, parentId };
+        })
+      );
+      return [].concat.apply([], arr);
     }
+    return [];
+  };
 
   destructureNodes = (json: ICV) => {
-    if (json.nodes){
-      return json.nodes.map(node => {
-        const {id, col, x, y} = node
-        return node
-      })
+    if (json.nodes) {
+      return json.nodes.map(node =>
+        (({ id, x, y, col }) => ({ id, x, y, col }))(node)
+      );
+      return json.nodes;
     }
     return [];
   };
@@ -103,8 +114,8 @@ destructureParagraphs = (json: ICV) => {
   //   };
 
   render() {
-    console.log(this.state);
-    console.log(this.state.nodes);
+    // console.log(this.state);
+    console.log(this.state.paragraphs);
     return (
       <Provider
         value={{
