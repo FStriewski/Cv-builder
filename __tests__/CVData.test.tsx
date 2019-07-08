01369adef24ee4fd629ac20c1a0587fb492df22a
Friedrich Striewski <friedrich.striewski@bynder.com>
2019-07-08 14:02:01 +0200

    Maintenance

---
+++ __tests__/CVData.test.tsx
@@ -0,0 +1,10 @@
+import { mount } from 'enzyme';
+import * as React from 'react';
+
+import { initialState } from '../src/data/initialData';
+
+describe('CV Data', () => {
+  it("can update an element's text color", async () => {
+    expect(true).toBe(true);
+  });
+});
